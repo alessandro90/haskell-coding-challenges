c3b35000e09b2be190c902045727e35b10395cf5
alessandro90 <alessandro.ceccato.3@gmail.com>
2025-04-04 22:12:19 +0200

    add json exe: show multiple json from command line
    also add readme

--- app/JsonApp.hs
+++ app/JsonApp.hs
@@ -1,4 +1,25 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "JSON"
+import Data.Bifunctor (Bifunctor (second))
+import qualified Data.Text.IO as T
+import Json (parseJson)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure, ExitSuccess))
+import System.IO.Error (catchIOError)
+import Text.Pretty.Simple (pPrint)
+
+main :: IO ExitCode
+main = runMain $ do
+  files <- mapM (\fname -> (fname,) <$> T.readFile fname) <$> getArgs
+  results <- fmap (second parseJson) <$> files
+  mapM_ formatResult results
+  pure ExitSuccess
+  where
+    runMain :: IO ExitCode -> IO ExitCode
+    runMain action =
+      let reportAndFail e = putStrLn ("ERROR: " <> show e) >> pure (ExitFailure 1)
+       in action `catchIOError` reportAndFail
+
+    formatResult (fname, parseResult) =
+      putStrLn ("Result of parsing file: '" <> fname <> "':\n")
+        >> either print pPrint parseResult
---
+++ readme.md
@@ -0,0 +1,4 @@
+# [Code Challenges](https://codingchallenges.fyi/challenges/intro/)
+
+- [x] [wc](https://codingchallenges.fyi/challenges/challenge-wc)
+- [x] [json](https://codingchallenges.fyi/challenges/challenge-json-parser)
