ccc4dc5a129edb27709746e21c11f396c9102858
alessandro90 <alessandro.ceccato.3@gmail.com>
2025-04-04 22:43:38 +0200

    JsonApp: return error if no files provided

--- app/JsonApp.hs
+++ app/JsonApp.hs
@@ -1,7 +1,11 @@
 module Main where
 
+import Control.Exception (throwIO)
+import Control.Monad (when)
 import Data.Bifunctor (Bifunctor (second))
+import qualified Data.List as L
 import qualified Data.Text.IO as T
+import GHC.IO.Exception ()
 import Json (parseJson)
 import System.Environment (getArgs)
 import System.Exit (ExitCode (ExitFailure, ExitSuccess))
@@ -10,12 +14,11 @@ import Text.Pretty.Simple (pPrint)
 
 main :: IO ExitCode
 main = runMain $ do
-  files <- mapM (\fname -> (fname,) <$> T.readFile fname) <$> getArgs
-  results <- fmap (second parseJson) <$> files
-  mapM_ formatResult results
+  files <- getArgs >>= mapM (\fname -> (fname,) <$> T.readFile fname)
+  when (L.null files) $ throwIO $ userError "Provide at least a file to parse"
+  mapM_ (formatResult . second parseJson) files
   pure ExitSuccess
   where
-    runMain :: IO ExitCode -> IO ExitCode
     runMain action =
       let reportAndFail e = putStrLn ("ERROR: " <> show e) >> pure (ExitFailure 1)
        in action `catchIOError` reportAndFail
