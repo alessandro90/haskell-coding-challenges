a2b1c797f4b3936cae56afcefaaee4d26f07c238
alessandro90 <alessandro.ceccato.3@gmail.com>
2025-04-14 19:44:00 +0200

    add Functor implementation for Tree. make Tree polymorphic

--- src/Huffman.hs
+++ src/Huffman.hs
@@ -1,5 +1,6 @@
 module Huffman where
 
+import Data.Bifunctor (Bifunctor (second))
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Internal as BSI
@@ -8,40 +9,36 @@ import Data.PQueue.Min (MinQueue)
 import qualified Data.PQueue.Min as PQ
 import Data.Word (Word8)
 
-data Tree
-  = Node Int Tree Tree
-  | Leaf (Int, Word8)
+data Tree a
+  = Node Int (Tree a) (Tree a)
+  | Leaf (Int, a)
   deriving (Eq, Show)
 
-leaf :: Int -> Char -> Tree
+leaf :: Int -> Char -> Tree Word8
 leaf n c = Leaf (n, BSI.c2w c)
 
-instance Ord Tree where
+instance (Eq a) => Ord (Tree a) where
   (Leaf (f, _)) `compare` (Leaf (f', _)) = f `compare` f'
   (Leaf (f, _)) `compare` (Node n' _ _) = f `compare` n'
   (Node n _ _) `compare` (Leaf (n', _)) = n `compare` n'
   (Node n _ _) `compare` (Node n' _ _) = n `compare` n'
 
-instance Semigroup Tree where
+instance Semigroup (Tree a) where
   ll@(Leaf (n, _)) <> ll'@(Leaf (n', _)) = Node (n + n') ll ll'
   ll@(Leaf (n, _)) <> nn@(Node n' _ _) = Node (n + n') ll nn
   nn@(Node n _ _) <> nn'@(Node n' _ _) = Node (n + n') nn nn'
   nn@(Node n _ _) <> ll@(Leaf (n', _)) = Node (n + n') nn ll
 
-countFreq :: ByteString -> MinQueue Tree
+instance Functor Tree where
+  fmap f (Leaf v) = Leaf $ second f v
+  fmap f (Node v l r) = Node v (fmap f l) (fmap f r)
+
+countFreq :: ByteString -> MinQueue (Tree Word8)
 countFreq bs =
   let m' = BS.foldr' (\b m -> M.insertWith (+) b 1 m) M.empty bs
    in M.foldrWithKey (\k v q -> PQ.insert (Leaf (v, k)) q) PQ.empty m'
 
--- buildTree :: MinQueue Tree -> Maybe Tree
--- buildTree queue = case PQ.minView queue of
---   Nothing -> Nothing
---   Just (t, q) ->
---     case PQ.minView q of
---       Nothing -> Just t
---       Just (t', q') -> buildTree $ PQ.insert (t <> t') q'
-
-buildTree :: MinQueue Tree -> Maybe Tree
+buildTree :: (Eq a) => MinQueue (Tree a) -> Maybe (Tree a)
 buildTree queue = PQ.minView queue >>= go
   where
     go (t, q) = case PQ.minView q of
--- test/HuffmanTest.hs
+++ test/HuffmanTest.hs
@@ -2,8 +2,9 @@
 
 module HuffmanTest where
 
+import Data.ByteString.Internal (w2c)
 import qualified Data.PQueue.Min as PQ
-import Huffman (Tree (Node), buildTree, countFreq, leaf)
+import Huffman (Tree (Leaf, Node), buildTree, countFreq, leaf)
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -80,55 +81,83 @@ treeMergeTest =
         assertEqual "t1 and t2 should be equal" expected merged
     ]
 
-treeCreationTest :: TestTree
-treeCreationTest =
-  testGroup
-    "huffman-tree-creation"
-    [ testCase "huffman-tree-creation-from-list" $ do
-        let l =
-              [ leaf 32 'C',
-                leaf 42 'D',
-                leaf 120 'E',
-                leaf 7 'K',
-                leaf 43 'L',
-                leaf 24 'M',
-                leaf 37 'U',
-                leaf 2 'Z'
-              ]
-            tree = buildTree $ PQ.fromList l
-            expected =
-              Node
-                307
-                (leaf 120 'E')
+treeFunctorTest :: TestTree
+treeFunctorTest =
+  testCase "huffman-tree-functor-test" $ do
+    let tree =
+          Node
+            2
+            (leaf 3 'A')
+            ( Node
+                4
                 ( Node
-                    187
-                    ( Node
-                        79
-                        (leaf 37 'U')
-                        (leaf 42 'D')
-                    )
-                    ( Node
-                        108
-                        (leaf 43 'L')
-                        ( Node
-                            65
-                            (leaf 32 'C')
-                            ( Node
-                                33
-                                ( Node
-                                    9
-                                    (leaf 2 'Z')
-                                    (leaf 7 'K')
-                                )
-                                (leaf 24 'M')
-                            )
-                        )
-                    )
+                    7
+                    (leaf 8 'I')
+                    (leaf 1 'Y')
                 )
-        case tree of
-          Nothing -> assertFailure "A tree is expected"
-          Just t -> assertEqual "trees should be equal" expected t
-    ]
+                (leaf 10 'P')
+            )
+        expected =
+          Node
+            2
+            (Leaf (3, 'A'))
+            ( Node
+                4
+                ( Node
+                    7
+                    (Leaf (8, 'I'))
+                    (Leaf (1, 'Y'))
+                )
+                (Leaf (10, 'P'))
+            )
+        tree' = w2c <$> tree
+    assertEqual "tree of word8 to tree of char" expected tree'
+
+treeCreationTest :: TestTree
+treeCreationTest = testCase "huffman-tree-creation-from-list" $ do
+  let l =
+        [ leaf 32 'C',
+          leaf 42 'D',
+          leaf 120 'E',
+          leaf 7 'K',
+          leaf 43 'L',
+          leaf 24 'M',
+          leaf 37 'U',
+          leaf 2 'Z'
+        ]
+      tree = buildTree $ PQ.fromList l
+      expected =
+        Node
+          307
+          (leaf 120 'E')
+          ( Node
+              187
+              ( Node
+                  79
+                  (leaf 37 'U')
+                  (leaf 42 'D')
+              )
+              ( Node
+                  108
+                  (leaf 43 'L')
+                  ( Node
+                      65
+                      (leaf 32 'C')
+                      ( Node
+                          33
+                          ( Node
+                              9
+                              (leaf 2 'Z')
+                              (leaf 7 'K')
+                          )
+                          (leaf 24 'M')
+                      )
+                  )
+              )
+          )
+  case tree of
+    Nothing -> assertFailure "A tree is expected"
+    Just t -> assertEqual "trees should be equal" expected t
 
 huffmanTests :: TestTree
 huffmanTests =
@@ -136,5 +165,6 @@ huffmanTests =
     "huffman"
     [ countFreqTest,
       treeMergeTest,
-      treeCreationTest
+      treeCreationTest,
+      treeFunctorTest
     ]
